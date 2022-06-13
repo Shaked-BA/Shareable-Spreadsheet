7f3384990238c611eddb104035f35a581069c780
Shaked-BA <benahash@post.bgu.ac.il>
2022-06-13 19:25:17 +0300

    Fixed outputs.

--- Shareable-Spreadsheet/Simulator.cs
+++ Shareable-Spreadsheet/Simulator.cs
@@ -8,8 +8,6 @@ class Simulator
     static private long cols;
     static private long wordsRows;
     static private long wordsCols;
-    //static private int originalRows;
-    //static private int originalCols;
     static private int nThreads;
     static private int nOperations;
     static private int mssleep;
@@ -25,17 +23,14 @@ class Simulator
         initVars(int.Parse(args[0]), int.Parse(args[1]), int.Parse(args[2]), int.Parse(args[3]), int.Parse(args[4]));
         threadsStart();
         threadsJoin();
-        sheet.save("C:/Users/shake/Desktop/sheet.txt");
     }
 
     static private void initVars(int rowsNum, int colsNum, int threadsNum, int operations, int ms)
     {
         rows = rowsNum;
         wordsRows = rowsNum;
-        //originalRows = rowsNum;
         cols = colsNum;
         wordsCols = colsNum;
-        //originalCols = colsNum;
         nThreads = threadsNum;
         mssleep = ms;
         nOperations = operations;
@@ -87,11 +82,11 @@ class Simulator
                 int c0 = random.Next(0, (int)Interlocked.Read(ref cols));
                 try
                 {
-                    Console.WriteLine("User[{0}]:[getCell] string \"{1}\" found in cell [{2},{3}].", DateTime.Now.ToString("h:mm:ss"), sheet.getCell(r0, c0), r0, c0);
+                    Console.WriteLine("User[{0}]:[{4}] string \"{1}\" found in cell [{2},{3}].", Thread.CurrentThread.Name, sheet.getCell(r0, c0), r0, c0, DateTime.Now.ToString("h:mm:ss"));
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("User[{0}]:[getCell] ---Failed--- find string in cell[{1},{2}].\n\tReason: {3}", DateTime.Now.ToString("h:mm:ss"), r0, c0, e.Message);
+                    Console.WriteLine("User[{0}]:[{4}] ---Failed--- find string in cell[{1},{2}].\n\tReason: {3}", Thread.CurrentThread.Name, r0, c0, e.Message, DateTime.Now.ToString("h:mm:ss"));
                 }
                 break;
             case 1: // setCell
@@ -101,11 +96,11 @@ class Simulator
                 try
                 {
                     sheet.setCell(r1, c1, str1);
-                    Console.WriteLine("User[{0}]:[setCell] string \"{1}\" inserted in cell [{2},{3}].", DateTime.Now.ToString("h:mm:ss"), str1, r1, c1);
+                    Console.WriteLine("User[{0}]:[{4}] string \"{1}\" inserted in cell [{2},{3}].", Thread.CurrentThread.Name, str1, r1, c1);
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("User[{0}]:[setCell] ---Failed--- insert string \"{1}\" in cell[{2},{3}].\n\tReason: {4}", DateTime.Now.ToString("h:mm:ss"), str1, r1, c1, e.Message);
+                    Console.WriteLine("User[{0}]:[{5}] ---Failed--- insert string \"{1}\" in cell[{2},{3}].\n\tReason: {4}", Thread.CurrentThread.Name, str1, r1, c1, e.Message, DateTime.Now.ToString("h:mm:ss"));
                 }
                 break;
             case 2: // searchString
@@ -113,11 +108,11 @@ class Simulator
                 try
                 {
                     Tuple<int, int> res2 = sheet.searchString(str2);
-                    Console.WriteLine("User[{0}]:[searchString] string \"{1}\" was found in cell [{2},{3}].", DateTime.Now.ToString("h:mm:ss"), str2, res2.Item1, res2.Item2);
+                    Console.WriteLine("User[{0}]:[{4}] string \"{1}\" was found in cell [{2},{3}].", Thread.CurrentThread.Name, str2, res2.Item1, res2.Item2, DateTime.Now.ToString("h:mm:ss"));
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("User[{0}]:[searchString] string \"{1}\" wasn't found.\n\tReason: {2}", DateTime.Now.ToString("h:mm:ss"), str2, e.Message);
+                    Console.WriteLine("User[{0}]:[{3}] string \"{1}\" wasn't found.\n\tReason: {2}", Thread.CurrentThread.Name, str2, e.Message, DateTime.Now.ToString("h:mm:ss"));
                 }
                 break;
             case 3: // exchangeRows
@@ -126,11 +121,11 @@ class Simulator
                 try
                 {
                     sheet.exchangeRows(r13, r23);
-                    Console.WriteLine("User[{0}]:[exchangeRows] rows {1} and {2} exchanged successfully.", DateTime.Now.ToString("h:mm:ss"), r13, r23);
+                    Console.WriteLine("User[{0}]:[{3}] rows {1} and {2} exchanged successfully.", Thread.CurrentThread.Name, r13, r23, DateTime.Now.ToString("h:mm:ss"));
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("User[{0}]:[exchangeRows] ---Failed--- exchange rows {1} and {2}.\n\tReason: {3}", DateTime.Now.ToString("h:mm:ss"), r13, r23, e.Message);
+                    Console.WriteLine("User[{0}]:[{4}] ---Failed--- exchange rows {1} and {2}.\n\tReason: {3}", Thread.CurrentThread.Name, r13, r23, e.Message, DateTime.Now.ToString("h:mm:ss"));
                 }
                 break;
             case 4: // exchangeCols
@@ -139,41 +134,37 @@ class Simulator
                 try
                 {
                     sheet.exchangeCols(c14, c24);
-                    Console.WriteLine("User[{0}]:[exchangeCols] columns {1} and {2} exchanged successfully.", DateTime.Now.ToString("h:mm:ss"), c14, c24);
+                    Console.WriteLine("User[{0}]:[{3}] columns {1} and {2} exchanged successfully.", Thread.CurrentThread.Name, c14, c24, DateTime.Now.ToString("h:mm:ss"));
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("User[{0}]:[exchangeCols] ---Failed--- exchange columns {1} and {2}.\n\tReason: {3}", DateTime.Now.ToString("h:mm:ss"), c14, c24, e.Message);
+                    Console.WriteLine("User[{0}]:[{4}] ---Failed--- exchange columns {1} and {2}.\n\tReason: {3}", Thread.CurrentThread.Name, c14, c24, e.Message, DateTime.Now.ToString("h:mm:ss"));
                 }
                 break;
             case 5: // searchInRow
                 int r5 = random.Next(0, (int) Interlocked.Read(ref rows));
                 String str5 = words[random.Next(0, (int) Interlocked.Read(ref wordsRows)), random.Next(0, (int) Interlocked.Read(ref wordsCols))];
-                //int r5 = random.Next(0, wordsRows);
-                //String str5 = words[r5, random.Next(0, wordsCols)];
                 try
                 {
                     int c5 = sheet.searchInRow(r5, str5);
-                    Console.WriteLine("User[{0}]:[searchInRow] string \"{1}\" was found in cell [{2},{3}].", DateTime.Now.ToString("h:mm:ss"), str5, r5, c5);
+                    Console.WriteLine("User[{0}]:[{4}] string \"{1}\" was found in cell [{2},{3}].", Thread.CurrentThread.Name, str5, r5, c5, DateTime.Now.ToString("h:mm:ss"));
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("User[{0}]:[searchInRow] ---Failed--- string \"{1}\" wasn't found in row {2}.\n\tReason: {3}\n" + e.StackTrace, DateTime.Now.ToString("h:mm:ss"), str5, r5, e.Message);
+                    Console.WriteLine("User[{0}]:[{4}] ---Failed--- string \"{1}\" wasn't found in row {2}.\n\tReason: {3}", Thread.CurrentThread.Name, str5, r5, e.Message, DateTime.Now.ToString("h:mm:ss"));
                 }
                 break;
             case 6: // searchInCol
                 int c6 = random.Next(0, (int) Interlocked.Read(ref cols));
                 String str6 = words[random.Next(0, (int) Interlocked.Read(ref wordsRows)), random.Next(0, (int) Interlocked.Read(ref wordsCols))];
-                //int c6 = random.Next(0, wordsCols);
-                //String str6 = words[random.Next(0, wordsRows), random.Next(0, wordsCols)];
                 try
                 {
                     int r6 = sheet.searchInCol(c6, str6);
-                    Console.WriteLine("User[{0}]:[searchInCol] string \"{1}\" was found in cell [{2},{3}].", DateTime.Now.ToString("h:mm:ss"), str6, r6, c6);
+                    Console.WriteLine("User[{0}]:[{4}] string \"{1}\" was found in cell [{2},{3}].", Thread.CurrentThread.Name, str6, r6, c6, DateTime.Now.ToString("h:mm:ss"));
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("User[{0}]:[searchInCol] ---Failed--- string \"{1}\" wasn't found in column {2}.\n\tReason: {3}\n" + e.StackTrace, DateTime.Now.ToString("h:mm:ss"), str6, c6, e.Message);
+                    Console.WriteLine("User[{0}]:[{4}] ---Failed--- string \"{1}\" wasn't found in column {2}.\n\tReason: {3}", Thread.CurrentThread.Name, str6, c6, e.Message, DateTime.Now.ToString("h:mm:ss"));
                 }
                 break;
             case 7: // searchInRange
@@ -182,19 +173,14 @@ class Simulator
                 int r27 = random.Next(0, (int) Interlocked.Read(ref rows));
                 int c27 = random.Next(0, (int) Interlocked.Read(ref cols));
                 String str7 = words[random.Next(0, (int) Interlocked.Read(ref wordsRows)), random.Next(0, (int) Interlocked.Read(ref wordsCols))];
-                //int r17 = random.Next(0, wordsRows);
-                //int c17 = random.Next(0, wordsCols);
-                //int r27 = random.Next(r17, wordsRows);
-                //int c27 = random.Next(0, wordsCols);
-                //String str7 = words[random.Next(r17, r27 + 1), random.Next(c17, wordsCols)];
                 try
                 {
                     Tuple<int, int> res7 = sheet.searchInRange(c17, c27, r17, r27, str7);
-                    Console.WriteLine("User[{0}]:[searchInRange] string \"{1}\" was found in cell [{2},{3}].", DateTime.Now.ToString("h:mm:ss"), str7, res7.Item1, res7.Item2);
+                    Console.WriteLine("User[{0}]:[{4}] string \"{1}\" was found in cell [{2},{3}].", Thread.CurrentThread.Name, str7, res7.Item1, res7.Item2, DateTime.Now.ToString("h:mm:ss"));
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("User[{0}]:[searchInRange] ---Failed--- string \"{1}\" wasn't found in range [[{2},{3}]:[{4},{5}]].\n\tReason: {6}\n" + e.StackTrace, DateTime.Now.ToString("h:mm:ss"), str7, r17, c17, r27, c27, e.Message);
+                    Console.WriteLine("User[{0}]:[{6}] ---Failed--- string \"{1}\" wasn't found in range [[{2},{3}]:[{4},{5}]].\n\tReason: {6}", Thread.CurrentThread.Name, str7, r17, c17, r27, c27, e.Message, DateTime.Now.ToString("h:mm:ss"));
                 }
                 break;
             case 8: // addRow
@@ -202,13 +188,12 @@ class Simulator
                 try
                 {
                     sheet.addRow(r8);
-                    Console.WriteLine("User[{0}]:[addRow] a new row added after row {1}.", DateTime.Now.ToString("h:mm:ss"), r8);
+                    Console.WriteLine("User[{0}]:[{2}] a new row added after row {1}.", Thread.CurrentThread.Name, r8, DateTime.Now.ToString("h:mm:ss"));
                     Interlocked.Increment(ref rows);
-                    //wordsRows = rows < originalRows ? rows : originalRows;
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("User[{0}]:[addRow] ---Failed--- a new row wasn't added after row {1}.\n\tReason: {2}", DateTime.Now.ToString("h:mm:ss"), r8, e.Message);
+                    Console.WriteLine("User[{0}]:[{3}] ---Failed--- a new row wasn't added after row {1}.\n\tReason: {2}", Thread.CurrentThread.Name, r8, e.Message, DateTime.Now.ToString("h:mm:ss"));
                 }
                 break;
             case 9: // addCol
@@ -216,13 +201,12 @@ class Simulator
                 try
                 {
                     sheet.addCol(c9);
-                    Console.WriteLine("User[{0}]:[addCol] a new column added after column {1}.", DateTime.Now.ToString("h:mm:ss"), c9);
+                    Console.WriteLine("User[{0}]:[{2}] a new column added after column {1}.", Thread.CurrentThread.Name, c9, DateTime.Now.ToString("h:mm:ss"));
                     Interlocked.Increment(ref cols);
-                    //wordsCols = cols < originalCols ? cols : originalCols;
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("User[{0}]:[addCol] ---Failed--- a new column wasn't added after column {1}.\n\tReason: {2}", DateTime.Now.ToString("h:mm:ss"), c9, e.Message);
+                    Console.WriteLine("User[{0}]:[{3}] ---Failed--- a new column wasn't added after column {1}.\n\tReason: {2}", Thread.CurrentThread.Name, c9, e.Message, DateTime.Now.ToString("h:mm:ss"));
                 }
                 break;
             case 10: // findAll
@@ -233,11 +217,11 @@ class Simulator
                     String[] out10 = new String[res10.Length];
                     for (int i = 0; i < res10.Length; i++)
                         out10[i] = res10[i].ToString();
-                    Console.WriteLine("User[{0}]:[findAll] string \"{1}\" was found in cells [{2}].", DateTime.Now.ToString("h:mm:ss"), str10, String.Join(", ", out10));
+                    Console.WriteLine("User[{0}]:[{3}] string \"{1}\" was found in cells [{2}].", Thread.CurrentThread.Name, str10, String.Join(", ", out10), DateTime.Now.ToString("h:mm:ss"));
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("User[{0}]:[findAll] ---Failed--- string \"{1}\" wasn't found.\n\tReason: {2}\n" + e.StackTrace, DateTime.Now.ToString("h:mm:ss"), str10, e.Message);
+                    Console.WriteLine("User[{0}]:[{3}] ---Failed--- string \"{1}\" wasn't found.\n\tReason: {2}", Thread.CurrentThread.Name, str10, e.Message, DateTime.Now.ToString("h:mm:ss"));
                 }
                 break;
             case 11: // setAll
@@ -246,21 +230,21 @@ class Simulator
                 try
                 {
                     sheet.setAll(str111, str211, false);
-                    Console.WriteLine("User[{0}]:[setAll] all strings \"{1}\" were replaced with string \"{2}\".", DateTime.Now.ToString("h:mm:ss"), str111, str211);
+                    Console.WriteLine("User[{0}]:[{3}] all strings \"{1}\" were replaced with string \"{2}\".", Thread.CurrentThread.Name, str111, str211, DateTime.Now.ToString("h:mm:ss"));
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("User[{0}]:[setAll] ---Failed--- strings \"{1}\" weren't replaced with strings \"{2}\".\n\tReason: {3}\n" + e.StackTrace, DateTime.Now.ToString("h:mm:ss"), str111, str211, e.Message);
+                    Console.WriteLine("User[{0}]:[{4}] ---Failed--- strings \"{1}\" weren't replaced with strings \"{2}\".\n\tReason: {3}", Thread.CurrentThread.Name, str111, str211, e.Message, DateTime.Now.ToString("h:mm:ss"));
                 }
                 break;
             case 12: // getSize
                 try
                 {
-                    Console.WriteLine("User[{0}]:[getSize] spreadsheet's size is {1}.", DateTime.Now.ToString("h:mm:ss"), sheet.getSize());
+                    Console.WriteLine("User[{0}]:[{2}] spreadsheet's size is {1}.", Thread.CurrentThread.Name, sheet.getSize(), DateTime.Now.ToString("h:mm:ss"));
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("User[{0}]:[getSize] ---Failed--- couldn't get spreadsheet's size.\n\tReason: {1}", DateTime.Now.ToString("h:mm:ss"), e.Message);
+                    Console.WriteLine("User[{0}]:[{2}] ---Failed--- couldn't get spreadsheet's size.\n\tReason: {1}", Thread.CurrentThread.Name, e.Message, DateTime.Now.ToString("h:mm:ss"));
                 }
                 break;
         }
